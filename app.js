1f2a6c2653053337e9656af2e428b029f9fe9ee8
alex-lenk <alexlenk.ru@gmail.com>
2021-09-20 11:30:52 +0300

    Added gitignore

--- app.js
+++ app.js
@@ -1,85 +1,51 @@
-const h = Vue.h
-
 const App = Vue.createApp({
-    data() {
-        return {
-            title: 'Hello',
-            title2: 'Hello'
-        }
-    },
-    methods: {
-        changeTitle() {
-            console.log(this)
-            this.title = 'Изменненый текст'
-        }
-    },
-    /*    template: `
-        <div class="card mb-5">
-            <h1>{{ title }}</h1>
-            <button class="btn primary" @click="title = 'Изменненый текст'">Change text</button>
-        </div>
-        `*/
-    render() {
-        return h(
-            'div', {
-                class: 'card'
-            }, [
-                h(
-                    'h1',
-                    {},
-                    this.title
-                ),
-                h(
-                    'button',
-                    {
-                        class: 'btn primary',
-                        onClick: this.changeTitle
-                    },
-                    'Change text'
-                )
-            ]
-        )
+  data() {
+    return {
+      activeIndex: 0,
+      steps: [
+        {
+          title: 'Основы',
+          text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'
+        },
+        {
+          title: 'Компоненты',
+          text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'
+        },
+        {
+          title: 'Роутер',
+          text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.'
+        },
+        {
+          title: 'Vuex',
+          text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.'
+        },
+        {
+          title: 'Composition',
+          text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.'
+        },
+      ]
     }
+  },
+  methods: {
+    prev() {
+      // когда нажимаем кнопку назад
+    },
+    reset() {
+      // начать заново
+    },
+    nextOfFinish() {
+      // кнопка вперед или закончить
+    },
+    setActive(idx) {
+      // когда нажимаем на определенный шаг
+    }
+  },
+  computed: {
+    // тут стоит определить несколько свойств:
+    // 1. текущий выбранный шаг
+    // 2. выключена ли кнопка назад
+    // 3. находимся ли мы на последнем шаге
+  }
 });
 
 App.mount('#app')
-
-let title = 'Vue js 3',
-    message = 'Учу: ' + title
-
-/*
-console.log(message)
-
-title = 'Angelar'
-
-console.log(message)
-*/
-/*
-const data = {
-    title: 'Vue js 3',
-    message: 'Учу: '
-}
-console.log(data)
-
-const proxy = new Proxy(data, {
-    get(target, p) {
-        console.log(target)
-        console.log(p)
-    }
-})
-console.log(proxy)
-
-proxy.message
-*/
-
-const person = {
-    name: 'Alex',
-    age: 34,
-    work: 'developer'
-}
-
-const objectProxy = new Proxy(person, {
-    get(target, prop) {
-        return target[prop]
-    }
-})
