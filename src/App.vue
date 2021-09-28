3ed6b1efecb8cc20cbf5ef60d9c7cc6bb40a4b16
alexlenk.ru <alexlenk.ru@gmail.com>
2021-09-28 09:43:45 +0300

    Created AppNews component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="card">
+      <h2>Актуальные новости на {{ localeDate }}</h2>
+      Открыто новостей: {{ newsRate }}
+    </div>
+
+    <app-news
+      v-for="(item, idx) in news" item="item"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-opened="item.isOpened"
+      :index="idx"
+      v-on:open-news="newsRate++"
+    ></app-news>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppNews from './AppNews'
 
 export default {
-  name: 'App',
+  data () {
+    return {
+      newsRate: 0,
+      localeDate: new Date().toLocaleDateString(),
+      news: [
+        {
+          title: 'Первое беспилотное такси в России',
+          id: 1,
+          isOpened: false
+        },
+        {
+          title: 'В мире прекратили продажу этилированного бензина',
+          id: 2,
+          isOpened: false
+        },
+        {
+          title: 'Грибы защитят космонавтов от радиации',
+          id: 3,
+          isOpened: false
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    'app-news': AppNews
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/AppNews.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <h3>{{ id }}. {{ title }} ({{ index }})</h3>
+    <button class="btn" @click="open">{{ isOpened ? 'Скрыть новость' : 'Раскрыть новость' }}</button>
+    <p v-if="isOpened">loream imsipet</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    isOpened: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator (value) {
+        return value === true || value === false
+      }
+    },
+    index: Number
+  },
+  data () {
+    return {
+      isNewsOpen: this.isOpened
+    }
+  },
+  methods () {
+  }
+}
+</script>
