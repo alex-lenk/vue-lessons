326ab18395ff9d543de0fc1ebe62fa243ce394ee
alex-lenk <alexlenk.ru@gmail.com>
2021-09-28 21:15:10 +0300

    Created example slots

---
+++ src/AppBlock.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot name="header"></slot>
+    <slot name="default">Нет текста</slot>
+    <slot name="footer" v-if="$slots.footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBlock',
+  mounted () {
+    console.log(this.$slots)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/AppExampleSlots.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" v-cloak>
+    <div class="card">
+      <h2>Slots</h2>
+    </div>
+
+    <app-block>
+      <p>Текст для нового блока</p>
+
+      <template #header>
+        <h4>This title</h4>
+      </template>
+
+      <template v-slot:footer>
+        <hr>
+        <small>Copyright</small>
+      </template>
+    </app-block>
+  </div>
+</template>
+
+<script>
+import AppBlock from './AppBlock'
+
+export default {
+  components: {
+    AppBlock
+  }
+}
+</script>
+
+<style>
+[v-cloak] {
+  display: none;
+}
+</style>
