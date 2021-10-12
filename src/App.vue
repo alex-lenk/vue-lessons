9699e78f1854c1096976656de096033e6aca167b
alex-lenk <alexlenk.ru@gmail.com>
2021-10-12 07:03:01 +0300

    Created base data

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,60 @@
 <template>
   <div class="container" v-cloak>
-    <div class="card">
+    <form class="card" @submit.prevent="createdPerson">
       <h1>Работа с базой данных</h1>
-    </div>
+
+      <div class="form-control">
+        <label for="name">Имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <app-button class="primary" :disabled="name.length < 2">создать пользователя</app-button>
+    </form>
+
+    <app-people-list
+      :people="people"
+      @load="peopleLoad"
+    ></app-people-list>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import AppButton from './AppButton'
+import AppPeopleList from './AppPeopleList'
 
 export default {
   data () {
-    return {}
+    return {
+      name: '',
+      urlBase: 'https://vue-with-http-68465-default-rtdb.europe-west1.firebasedatabase.app/people.json',
+      people: []
+    }
   },
-  methods: {},
-  components: {}
+  methods: {
+    async peopleLoad () {
+      await axios.get(this.urlBase
+      )
+    },
+    async createdPerson () {
+      const response = await fetch(this.urlBase, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json
+
+      console.log(firebaseData)
+
+      this.name = ''
+    }
+  },
+  components: { AppButton, AppPeopleList }
 }
 </script>
 
---
+++ src/AppPeopleList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="people.length !== 0">
+    <div class="card" v-for="person in people" :key="person.id">
+      <h3>{{ person }}</h3>
+    </div>
+  </div>
+
+  <div class="card" v-else>
+    <h5>Людей пока нет</h5>
+
+    <button class="btn" @click="$emit('load')">подгрузить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['load'],
+  props: ['people'],
+  name: 'AppPeopleList'
+}
+</script>
