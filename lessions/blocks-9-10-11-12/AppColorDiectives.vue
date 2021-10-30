744cc8450035c02c3d6c4cf539a9d930713fa492
alex-lenk <alexlenk.ru@gmail.com>
2021-10-30 18:17:47 +0300

    Added directives learn

---
+++ lessions/blocks-9-10-11-12/AppColorDiectives.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container" v-cloak>
+    <div class="card" v-if="show">
+      <h4 v-color:[typeStyle].blink.hover="myColor">Создание директивы</h4>
+
+      <div class="form-control">
+        <label for="active-input">Активный</label>
+        <input type="text" id="active-input" v-focus>
+      </div>
+
+      <button class="btn" @click="myColor = 'darkblue'">поменять цвет</button>
+      <button class="btn primary" @click="changeStyle">поменять ТИП</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import focusDirective from "./js/directives/focusDirective";
+import colorsDirective from "./js/directives/colorsDirective";
+
+export default {
+  data() {
+    return {
+      myColor: 'darkred',
+      typeStyle: 'color',
+      show: true
+    }
+  },
+  methods: {
+    changeStyle() {
+      this.typeStyle = this.typeStyle === 'color' ? 'backgroundColor' : 'color'
+    }
+  },
+  mounted() {
+/*    setTimeout(()=> {
+      this.show = false
+    }, 20000)*/
+  },
+  directives: {
+    focus: focusDirective,
+    color: colorsDirective
+  }
+}
+</script>
+
+<style scoped>
+h4 {
+  border: 1px solid transparent;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="container" v-cloak>
     <div class="card">
-      <h4>Создание директивы</h4>
-
-      <div class="form-control">
-        <label for="active-input">Активный</label>
-        <input type="text" id="active-input" v-focus>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import focusDirective from "./js/directives/focusDirective";
-
 export default {
-  directives: {
-    focus: focusDirective
-  }
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+  },
+  directives: {}
 }
 </script>
