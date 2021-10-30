3169b928aba3da65aa5de47dc56fe3c54c1a7385
alexlenk.ru <alexlenk.ru@gmail.com>
2021-10-30 10:57:20 +0300

    Created directive

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="container" v-cloak>
     <div class="card">
+      <h4>Создание директивы</h4>
+
+      <div class="form-control">
+        <label for="active-input">Активный</label>
+        <input type="text" id="active-input" v-focus>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import focusDirective from "./js/directives/focusDirective";
+
 export default {
-  components: {}
+  directives: {
+    focus: focusDirective
+  }
 }
 </script>
