dabd7abe9f7c6b5c6bb78bbc0a510915e181f6b9
alex-lenk <alexlenk.ru@gmail.com>
2021-12-15 07:35:24 +0300

    Added reusable page

---
+++ src/AppAlert.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="alert primary">
+    <p class="alert-title">{{ title }}</p>
+    <p>This text</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'type'],
+  name: "AppAlert"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ReusablePage.vue
+++ src/views/ReusablePage.vue
@@ -1,30 +1,38 @@
 <template>
+  <app-alert v-if="alert" title="it is work" type="primary"></app-alert>
+
   <div class="card">
     <h1>Новая страница для примера</h1>
 
-    <button class="btn primary">показать сообщение</button>
+    <button class="btn primary" @click="toggle">{{ alert ? '' : 'показать'}} сообщение</button>
     <button class="btn" @click="nav">на главную</button>
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
 import {useRouter, useRoute} from 'vue-router'
+import AppAlert from "../AppAlert";
 
 export default {
   setup() {
+    const alert = ref(false)
     const router = useRouter()
     const route = useRoute()
 
     console.log(route.path)
 
-    const nav = () => {
-      router.push('/')
-    }
+    const nav = () => router.push('/')
+
+    const toggle = () => alert.value = !alert.value
 
     return {
-      nav
+      alert,
+      nav,
+      toggle
     }
   },
+  components: {AppAlert},
   name: "ReUsable"
 }
 </script>
