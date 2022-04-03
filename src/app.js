435322707a949a9f8dead0336fffecb6dd0d55cf
alex-lenk <alexlenk.ru@gmail.com>
2022-04-03 21:49:05 +0300

    Created begin task

---
+++ src/app.js
@@ -0,0 +1,38 @@
+Vue.createApp({
+  data() {
+    return {
+      title: 'Список',
+      placeholder: 'Введите название',
+      inputValue: '',
+      notes: ['value simple', 'value', 'one']
+    }
+  },
+  methods: {
+    addInputValue(event) {
+      if (this.inputValue !== '' && (event.key || !event.key)) {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+    },
+    deleteCurrentNote(idx) {
+      this.notes.shift(idx)
+      //event.target.closest('.list-item').remove()
+    },
+    toUppercase(item) {
+      return item.toUpperCase()
+    }
+  },
+  computed: {
+    doubleCounts() {
+      return this.notes.length * 2
+    }
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        console.log(value)
+        this.inputValue = ''
+      }
+    }
+  }
+}).mount('#app')
